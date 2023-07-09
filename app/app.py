1bd47e666838df7dd1e71d664ad2498abea1460d
=SarahMumbua <mumbuasarah2030@gmail.com>
2023-07-09 18:44:32 +0300

    added requirements.txt

--- app/app.py
+++ app/app.py
@@ -7,8 +7,9 @@ from config import Config
 from flask_cors import CORS
 
 app = Flask(__name__)
-#configure database connection : type of database management,user,password,host,database name
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/recipe_db'
+#configure database connection : type of database management,user,password,host,database_name
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:admin@localhost/recipe_db'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://taskmanagerdatabase_vbg5_user:OFZql5CvO1dZ95N9ME9waqC3DNTHoP4F@dpg-cilc1p95rnuvtgrja2c0-a.oregon-postgres.render.com/taskmanagerdatabase_vbg5'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config["JWT_TOKEN_LOCATION"] = ["headers"]
 app.config.from_object(Config)
